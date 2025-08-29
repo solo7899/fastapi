f810a73b3c3f5bf62ea6f7c8c3548f912802483d
solo7899 <dolati.hamidreza78@gmail.com>
2025-08-29 16:26:53 +0330

    setting up a post models and Relationship between User and Post models

--- src/models.py
+++ src/models.py
@@ -1,10 +1,20 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from pydantic import EmailStr
 
 
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    username: str = Field(nullable=False, index=True, unique=True)
-    email: EmailStr = Field(nullable=False, unique=True)
-    password: str = Field(nullable=False, min_length=8)
+    username: str = Field(index=True, unique=True)
+    email: EmailStr = Field(unique=True)
+    password: str = Field(min_length=8)
+
+    posts: list["Post"] | None = Relationship(back_populates="user")
+
+
+class Post(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    content: str = Field()
+
+    user_id: int = Field(foreign_key="user.id")
+    user: User|None = Relationship(back_populates="posts")
