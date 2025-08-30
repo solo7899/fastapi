0e787bd7e0d8f1ac5baa5c88ca2be2832a980a1d
solo7899 <dolati.hamidreza78@gmail.com>
2025-08-30 19:50:14 +0330

    added a datetime created_at field in database tables

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, Field, Relationship
 from pydantic import EmailStr
+from datetime import datetime
 
 
 
@@ -8,6 +9,7 @@ class User(SQLModel, table=True):
     username: str = Field(index=True, unique=True)
     email: EmailStr = Field(unique=True)
     password: str = Field(min_length=8)
+    created_at: datetime = Field(default_factory=datetime.now())
 
     posts: list["Post"] | None = Relationship(back_populates="user")
 
@@ -15,6 +17,7 @@ class User(SQLModel, table=True):
 class Post(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     content: str = Field()
+    created_at: datetime = Field(default_factory=datetime.now())
 
     user_id: int = Field(foreign_key="user.id")
     user: User|None = Relationship(back_populates="posts")
