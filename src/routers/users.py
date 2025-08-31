936116c7fa5c1ed84cd25edc7ce101dddfe48793
solo7899 <dolati.hamidreza78@gmail.com>
2025-08-31 15:57:08 +0330

    first commition in routers testing users get func

---
+++ src/routers/users.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from src import schemas
+
+
+router = APIRouter("/users", tags=["users"])
+
+users = [
+    {
+        'id': 1,
+        'username': 'hrd',
+        'email': 'hrd@gmail.com',
+        'password': 'password',
+        'created_at': '2025-06-15 14:30:00'
+    },
+    {
+        'id': 2,
+        'username': 'hrd_d',
+        'email': 'hrd_d@gmail.com',
+        'password': 'password',
+        'created_at': '2024-03-01 11:30:00'
+    }
+]
+@router.get("/", response_model=list[schemas.UserOut])
+async def get_users_list():
+    return users
