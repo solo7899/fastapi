51b8e1ca6e93d9044780057782e374261e671fbd
solo7899 <dolati.hamidreza78@gmail.com>
2025-08-31 23:39:10 +0330

    doing error handling for signup func

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,26 +1,11 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from src import schemas, db, models
 from sqlmodel import select
 
 
 router = APIRouter(prefix = "/users", tags=["users"])
 
-users = [
-    {
-        'id': 1,
-        'username': 'hrd',
-        'email': 'hrd@gmail.com',
-        'password': 'password',
-        'created_at': '2025-06-15 14:30:00'
-    },
-    {
-        'id': 2,
-        'username': 'hrd_d',
-        'email': 'hrd_d@gmail.com',
-        'password': 'password',
-        'created_at': '2024-03-01 11:30:00'
-    }
-]
+
 @router.get("/", response_model=list[schemas.UserOut])
 async def get_users_list(session: db.SessionDep):
     users = session.exec(select(models.User))
@@ -29,6 +14,12 @@ async def get_users_list(session: db.SessionDep):
 
 @router.post("/signup", response_model=schemas.UserOut, status_code=status.HTTP_201_CREATED)
 async def sign_up(user: schemas.UserSignUp, session: db.SessionDep):
+    #todo : error handling needed
+    check_username_or_email_existence = session.exec(select(models.User).where(
+        (user.username == models.User.username) | (models.User.email == user.email))).first()
+    if check_username_or_email_existence:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Username or email already in use")
+
     user = models.User(**user.model_dump())
     session.add(user)
     session.commit()
