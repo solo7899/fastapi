b348682829179817019239ac17bd89e17b05bcdd
solo7899 <dolati.hamidreza78@gmail.com>
2025-08-31 14:40:09 +0330

    UserSignUp Schema

--- src/schemas.py
+++ src/schemas.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel, Field, EmailStr
 
 class UserBase(BaseModel):
     username: str = Field(min_length=3, max_length=30)
+
+
+class UserSignUp(UserBase):
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=30)
