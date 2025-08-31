a83b42405ded3ae7686fa697f09d09f38e69ca40
solo7899 <dolati.hamidreza78@gmail.com>
2025-08-31 19:02:27 +0330

    troubleshoot: setting created_at to a callable func

--- src/models.py
+++ src/models.py
@@ -10,7 +10,7 @@ class User(SQLModel, table=True):
     username: str = Field(index=True, unique=True)
     email: EmailStr = Field(unique=True)
     password: str = Field(min_length=8)
-    created_at: datetime.datetime = Field(default_factory=datetime.datetime.now(datetime.timezone.utc))
+    created_at: datetime.datetime = Field(default_factory=lambda: datetime.datetime.now(datetime.timezone.utc))
 
     posts: list["Post"] | None = Relationship(back_populates="user")
 
@@ -18,7 +18,7 @@ class User(SQLModel, table=True):
 class Post(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     content: str = Field()
-    created_at: datetime.datetime = Field(default_factory=datetime.datetime.now(datetime.timezone.utc))
+    created_at: datetime.datetime = Field(default_factory=lambda: datetime.datetime.now(datetime.timezone.utc))
 
     user_id: int = Field(foreign_key="user.id")
     user: User|None = Relationship(back_populates="posts")
