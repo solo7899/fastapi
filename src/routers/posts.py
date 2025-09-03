e0147e3d349e41e903c498429bc9302573172336
solo7899 <dolati.hamidreza78@gmail.com>
2025-09-03 17:48:34 +0330

    list of all posts

--- src/routers/posts.py
+++ src/routers/posts.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Depends
+from sqlmodel import select
 from typing import Annotated
 
-from src import oauth2
+from src import oauth2, db, models, schemas
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
 
-@router.get("/")
-async def get_posts_list(user: Annotated[str, Depends(oauth2.get_current_user)]):
-    return "List of posts"
+@router.get("/", response_model=list[schemas.PostOut])
+async def get_posts_list(user: Annotated[str, Depends(oauth2.get_current_user)], session: db.SessionDep):
+    posts = session.exec(select(models.Post)).all()
+    return posts
+
