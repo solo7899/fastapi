1b1b83aff719a10d4d82ffefb1f574a80c4f9589
solo7899 <dolati.hamidreza78@gmail.com>
2025-09-03 18:54:13 +0330

    update post added

--- src/routers/posts.py
+++ src/routers/posts.py
@@ -1,3 +1,4 @@
+import datetime
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlmodel import select
 from typing import Annotated
@@ -33,3 +34,19 @@ async def delete_post(post_id: int, user: Annotated[models.User, Depends(oauth2.
     session.delete(post)
     session.commit()
     return Response(content="Message deleted successfully", status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.put("/update/{post_id}", response_model=schemas.PostOut)
+async def update_post(post_id: int, updated_post: schemas.PostIn, user: Annotated[models.User, Depends(oauth2.get_current_user)], session: db.SessionDep):
+    post = session.get(models.Post, post_id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    if post.user_id != user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+
+    post.content = updated_post.content
+    post.updated_at = datetime.datetime.now(datetime.timezone.utc)
+    session.add(post)
+    session.commit()
+    session.refresh(post)
+    return post
