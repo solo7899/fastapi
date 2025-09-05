6dbe27a248d17dee8cb225174fe03fb597d30156
solo7899 <dolati.hamidreza78@gmail.com>
2025-09-05 22:23:37 +0330

    added 2 new funcs; create_group and get a user groups]

--- src/routers/group.py
+++ src/routers/group.py
@@ -1,14 +1,28 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from typing import Annotated
 from sqlmodel import select
 
 from src import oauth2, models, db, schemas
 
 
-router = APIRouter(prefix="/group", tags=["group"])
+router = APIRouter(prefix="/groups", tags=["group"])
 
 @router.get("/", response_model=list[schemas.GroupOut])
 async def get_group_list(session: db.SessionDep, user: Annotated[models.User, Depends(oauth2.get_current_user)]):
     groups = session.exec(select(models.Group)).all()
     return groups
 
+
+@router.get("/{user_id}", response_model=list[schemas.GroupBase])
+async def get_user_groups(user_id: int, session: db.SessionDep, user: Annotated[models.User, Depends(oauth2.get_current_user)]):
+    user_groups = session.exec(select(models.Group).join(models.User.groups).where(models.User.id == user_id)).all()
+    return user_groups
+
+
+@router.post("/create", response_model=schemas.GroupOut, status_code=status.HTTP_201_CREATED)
+async def create_group(group: schemas.GroupIn, session: db.SessionDep, user: Annotated[models.User, Depends(oauth2.get_current_user)]):
+    new_group = models.Group(**group.model_dump())
+    session.add(new_group)
+    session.commit()
+    session.refresh(new_group)
+    return new_group
