c235d4cfce561687aaad2c05f7c5c1814ed04c3a
solo7899 <dolati.hamidreza78@gmail.com>
2025-09-05 22:40:21 +0330

    users can join to groups

--- src/routers/group.py
+++ src/routers/group.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from typing import Annotated
 from sqlmodel import select
 
@@ -26,3 +26,15 @@ async def create_group(group: schemas.GroupIn, session: db.SessionDep, user: Ann
     session.commit()
     session.refresh(new_group)
     return new_group
+
+@router.post("/{group_id}/join", response_model=schemas.GroupOut)
+async def join_group(group_id: int, session: db.SessionDep, user: Annotated[models.User, Depends(oauth2.get_current_user)]):
+    group = session.get(models.Group, group_id)
+    if not group:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Group not found")
+    if user in group.members:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Already a member of this group")
+    group.members.append(user)
+    session.commit()
+    session.refresh(group)
+    return group
